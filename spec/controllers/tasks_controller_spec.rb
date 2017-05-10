dcb4844f855c49c896870a6631f39254787fae80
roderickjoseph <rjgauvin@gmail.com>
2017-05-10 03:48:25 +0000

    Inital test passes, but don't know why

---
+++ spec/controllers/tasks_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe TasksController, type: :controller do
+  describe 'tasks.index' do
+    let!(:task1) { FactoryGirl.create :task }
+    # let!(:task2) { FactoryGirl.create :task }
+
+    it 'successfully returns the index page' do
+      # task1 = FactoryGirl.create(:task)
+      # task2 = FactoryGirl.create(:task)
+      get :index
+      expect(response).to have_http_status :success
+      # response_value = ActiveSupport::JSON.decode(response.body)
+      # expect(response_value.count).to eq(2)
+    end
+
+    # it 'responds with the two created test tasks' do
+    #   # task1 = FactoryGirl.create(:task)
+    #   # task2 = FactoryGirl.create(:task)
+    #   response_value = ActiveSupport::JSON.decode(@response.body)
+    #   expect(response_value.count).to eq(2)
+    # end
+  end
+end
