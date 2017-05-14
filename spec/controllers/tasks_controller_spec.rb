b4810ac5ba4e7162efd79161277854de4a8f566f
roderickjoseph <rjgauvin@gmail.com>
2017-05-14 01:34:58 +0000

    Added update to tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  describe 'tasks.index' do
-    let!(:task1) { FactoryGirl.create :task }
-    # let!(:task2) { FactoryGirl.create :task }
+  describe '.index' do
+    let!(:task1) { FactoryGirl.create :task1 }
+    # let!(:task2) { FactoryGirl.create :task2 }
 
     it 'successfully returns the index page' do
       # task1 = FactoryGirl.create(:task)
@@ -20,5 +20,15 @@ RSpec.describe TasksController, type: :controller do
     #   response_value = ActiveSupport::JSON.decode(@response.body)
     #   expect(response_value.count).to eq(2)
     # end
+  end
+
+  describe '.update' do
+    let(:task) { FactoryGirl.create :task1 }
+    it 'marks tasks as done' do
+      put :update, params: { id: task.id, task: { done: true } }
+      expect(response).to have_http_status(:success)
+      task.reload
+      expect(task.done).to eq(true)
+    end
   end
 end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
-  factory :task do
+  factory :task1, class: Task do
     title 'Do the dishes'
+    done false
+  end
+  factory :task2, class: Task do
+    title 'Do the laundry'
     done false
   end
 end
